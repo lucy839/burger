ac2d06b820376bab52e834d6c13fe19b448f21ae
Youngju Lee <lucylee839@hotmail.com>
2019-06-02 21:16:58 -0700

    added comments on models

--- models/burger.js
+++ models/burger.js
@@ -2,32 +2,21 @@
 var orm = require("../config/orm.js");
 
 var burger = {
+    // function to select all burgers
     selectAll: function(cb) {
       orm.selectAll("burgers", function(res) {
         cb(res);
       });
     },
-    // The variables cols and vals are arrays.
-    // insertOne: function(burger_name, cb) {
-    //   orm.insertOne("burgers", cols, vals, function(res) {
-    //     cb(res);
-    //   });
-    // },
-    // insertOne: function(burger_name, cb){
-    //   orm.insertOne(burger_name, function(res){
-    //     cb(res);
-    //   });
-    // },
+
+    // function to insert one new data
     insertOne: function(cols, vals, cb) {
       orm.insertOne("burgers", cols, vals, function(res) {
         cb(res);
       });
     },
-    // updateOne: function(objColVals, condition, cb) {
-    //   orm.updateOne("burgers", objColVals, condition, function(res) {
-    //     cb(res);
-    //   });
-    // }
+
+    // function to update one existing data
     updateOne: function(burger_id, callback){
       orm.updateOne("burgers",burger_id, function(res){
         callback(res);
